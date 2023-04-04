2f9df6d55479c3104e85c44fea8e7ec9748f4570
PierreCPE <pierre.gosson@cpe.fr>
2023-04-04 16:57:50 +0200

    chgmt nom dossiers

---
+++ main/site_web/flaskapp.py
@@ -0,0 +1,41 @@
+#!/usr/local/bin/python
+# -*- coding: UTF-8 -*-
+
+#pip install opencv-contrib-python
+
+
+from flask import Flask, render_template, Response
+import cv2
+
+
+
+def gen_frames():
+    cap = cv2.VideoCapture(0) # Replace 0 with your camera index if you have multiple cameras
+    nom="fenêtre caméra"
+    #largeur=int(cam.get(cv2.CAP_PROP_FRAME_WIDTH))
+    #hauteur=int(cam.get(cv2.CAP_PROP_FRAME_HEIGHT))
+    #cv2.namedWindow(nom, cv2.WND_PROP_FULLSCREEN)
+
+    while True:
+        ret, frame = cap.read()
+        if not ret:
+            break
+        ret,buffer = cv2.imencode('.jpg', frame)
+        frame = buffer.tobytes()
+        yield (b'--frame\r\n'b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+    #cap.release()
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('camera.html')
+
+@app.route('/videofeed')
+def videofeed():
+    return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+if __name__=="__main__" :
+    app.run(debug=True)
