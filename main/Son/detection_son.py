7e327df2892a30dbb29f7e5b434f0daefa5b994f
Capucine <capucine.jumelle@cpe.fr>
2023-04-06 10:33:55 +0200

    detect son 5 fois

--- main/Son/detection_son.py
+++ main/Son/detection_son.py
@@ -8,15 +8,15 @@ freq_min = 500
 freq_max = 4000
 
 # Taille de la fenetre d'analyse spéctrale en échantillons
-taille_fenetre = 502 
+taille_fenetre = 256 
 #Plus elle est grande, plus la resolution fréquentielle est fine mais plus la resolution temporelle sera mauvaise.
 
 # Pas entre chaque fenetre d'analyse
-pas = 250 
+pas = 128
 #Plus le pas est grand, plus l'analyse est fine mais plus elle est lente
 
 # Taille de la FFT
-taille_fft = 502 
+taille_fft = 512 
 # Elle permettra de determiner le nombre de points de la tranfomé de fourier. C'est la resolution fréquentielle. On note 
 # que taille_fft >= taille_fenetre.
 
@@ -27,29 +27,40 @@ taille_fft = 502
 Fs = 44100 
 
 # Durée de l'enregistrement en secondes
-duree = 10 
+duree = 3
 
 
 #ENREGISTREMENT
-print("Enregistrement en cours")
-signal = sd.rec(int(duree * Fs), samplerate=Fs, channels=1)
-sd.wait()
-print("Enregistrement terminé")
+spectres_moyen = []
+for i in range(5):
+    print("Enregistrement en cours")
+    signal = sd.rec(int(duree * Fs), samplerate=Fs, channels=1)
+    sd.wait()
+    print("Enregistrement terminé")
 
 
-f, t, S = sig.spectrogram(signal[:,0], fs=Fs, window='hann', nperseg=taille_fenetre, noverlap=taille_fenetre-pas, nfft=taille_fft, detrend=False)
+    f, t, S = sig.spectrogram(signal[:,0], fs=Fs, window='hann', nperseg=taille_fenetre, noverlap=taille_fenetre-pas, nfft=taille_fft, detrend=False)
 
 
-freq_bin = np.logical_and(f > freq_min, f <= freq_max)
-
-
-
-spectre_moyen = np.mean(np.abs(S[freq_bin, :]), axis=0)
+    freq_bin = np.logical_and(f > freq_min, f <= freq_max)
 
 
 
-seuil = 30 * np.std(spectre_moyen)
-if np.max(spectre_moyen) > seuil:
-    print('Bruit détecté, fuyons!')
-else:
-    print('Aucun bruit bizarre, restons bien caché!')
+    spectre_moyen = np.mean(np.abs(S[freq_bin, :]), axis=0)
+    spectres_moyen.append(spectre_moyen)
+
+    seuil = 10 * np.std(spectre_moyen)
+    if np.max(spectre_moyen) > seuil:
+        print('Bruit détecté, fuyons!')
+
+        if i>0:
+
+            seuil = 10 * np.std(spectre_moyen)
+            if np.max(spectre_moyen) > seuil:
+                print('Bruit de plus en plus fort!')
+                
+        else:
+            print('Le bruit se calme!')
+    
+    else:
+        print('Aucun bruit bizarre, restons bien caché!')
