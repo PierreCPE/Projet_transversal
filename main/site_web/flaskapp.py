07a68f58489152c5ff8bce32c901bc7f139566a4
Capucine <capucine.jumelle@cpe.fr>
2023-04-18 14:46:11 +0200

    flask affiche pt rg

--- main/site_web/flaskapp.py
+++ main/site_web/flaskapp.py
@@ -21,7 +21,7 @@ def gen_frames():
 
     centreX_video = largeur // 2
     centreY_video = hauteur // 2
-
+    cpt=0
     detection = True
     while True:
         if detection:
@@ -48,6 +48,40 @@ def gen_frames():
             cv2.line(image, (centreX_video, centreY_video - 10), (centreX_video, centreY_video + 10), couleur_ligne, epaisseur_ligne)
             cv2.line(image, (centreX_video - 10, centreY_video), (centreX_video + 10, centreY_video), couleur_ligne, epaisseur_ligne)
             ret,buffer = cv2.imencode('.jpg', image)
+
+
+            if cpt % 100 == 0: #Si cpt est un multiple de 100 alors on rentre dans la boucle. 
+                # Trouver le plus grand contour (l'objet rouge entouré de bleu)
+                surface_max=None #le contour ayant la plus grande surface dans l'image
+                val_surface_max=0 #la valeur de la surface  maximale trouvée
+                for contour in contours:
+                    surface_contour = cv2.contourArea(contour) #calcule la surface du contour courant 
+                    if surface_contour > val_surface_max:
+                        surface_max = contour #contiends le contour avec la plus grande surface
+                        val_surface_max = surface_contour #contiends la valeur de cette surface maximale
+            
+            #Si aucun contour n'a été détecté dans l'image, surface_max restera à None.
+
+            # Si un objet rouge entouré de bleu a été détecté, récupérer sa position et la comparer avec le centre de l'image
+            if np.all(surface_max) != None:
+                # Récupérer les coordonnées du rectangle englobant du plus grand contour
+                x, y, l, h = cv2.boundingRect(surface_max) #x et y sont les coordonnée en haut a gauche du rectangle. l et h sont la longueur et la hauteur du rectangle
+
+                # Calculer la position de l'objet par rapport au centre de l'image
+                centreX_rect = x + l / 2
+                centreY_rect = y + h / 2
+
+                x= centreX_rect -centreX_video
+                y= centreY_rect -centreY_video  
+                #x_norm = x/ largeur
+                #y_norm = y / hauteur   
+                #si x est positif, le robot doit tourner à droite. Plus x est grand, plus le centre de la video est loin de l'objet au sens de l'horizontale
+                #si y est positif, le robot doit baisser la tete. Plus y est grand, plus le centre de la video est loin de l'objet au sens de la verticale
+
+                print('')
+                print("Coordonnée de", x, "et de " ,y,"par rapport au centre")
+                print('')
+
 
             frame = buffer.tobytes()
             # Afficher la trame courante avec les contours dans une fenêtre de sortie
