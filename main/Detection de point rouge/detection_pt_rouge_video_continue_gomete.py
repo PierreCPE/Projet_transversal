bfa0214f4f98cc23956764a35af1eae7b3eece18
Capucine <capucine.jumelle@cpe.fr>
2023-04-18 14:49:53 +0200

    de pt rg

--- main/Detection de point rouge/detection_pt_rouge_video_continue_gomete.py
+++ main/Detection de point rouge/detection_pt_rouge_video_continue_gomete.py
@@ -80,8 +80,7 @@ while True:
 
             x= centreX_rect -centreX_video
             y= centreY_rect -centreY_video  
-            #x_norm = x/ largeur
-            #y_norm = y / hauteur   
+            
             #si x est positif, le robot doit tourner à droite. Plus x est grand, plus le centre de la video est loin de l'objet au sens de l'horizontale
             #si y est positif, le robot doit baisser la tete. Plus y est grand, plus le centre de la video est loin de l'objet au sens de la verticale
 
