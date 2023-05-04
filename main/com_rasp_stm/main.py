20e0084caa81bacc34c8effb396e124bfb3e2522
PierreCPE <pierre.gosson@cpe.fr>
2023-05-04 09:08:29 +0200

    com stm32 pc v1

--- main/com_rasp_stm/main.py
+++ main/com_rasp_stm/main.py
@@ -2,9 +2,20 @@
 import serial
 import time
 
-serial=serial.Serial('/dev/ttyUSB0',115200) #On defini un objet serial avec l'adresse du port et la vitesse
+serial=serial.Serial('COM6',115200) #On defini un objet serial avec l'adresse du port et la vitesse
+if not serial.isOpen(): #Est ce que le port est open si non alors on l'ouvre.
+    serial.open()
+print('com is open', serial.isOpen())
 
 while True: #on effectue une boucle infinie
     serial.flushInput();serial.flushOutput() #On nettoie les buffers
-    number==(serial.readLine().decode("utf8",errors="replace")) #On lit sur le port serie et on affecte dans une variable
+    message = "salut$"
+    b = message.encode('utf-8') #Encodage en byte.
+    print("message envoyé : ")
+    print(b)
+    serial.write(b)
+    number = (serial.readline().decode("utf8",errors="replace")) #On lit sur le port serie et on affecte dans une variable
+    print("message recu : ")
     print(number) #On imprime dans la console
+    
+
