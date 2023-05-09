1b097fbad15a1e3cfdc4339143183cc14354365f
Hugues <hugues.farthouat@cpe.fr>
2023-05-09 16:59:56 +0200

    re

--- main/site_web/app.py
+++ main/site_web/app.py
@@ -42,7 +42,7 @@ class App:
         ###########################################
         config = ThreadSafeDict()
         config['detection_contour'] = True
-        config['serial'] = False # Activer ou non le port serial
+        config['serial'] = True # Activer ou non le port serial
         # config['serial_port'] = 'COM8' # Port série
         config['serial_port'] = '/dev/ttyUSB0' # Port série
         config['serial_baudrate'] = 115200 # Baudrate du port série
