1a826f2c812334f96da8dc0fa1d7e0dd7931eedf
Capucine <capucine.jumelle@cpe.fr>
2023-05-16 16:08:10 +0200

    son implementation

--- main/site_web/robotserver.py
+++ main/site_web/robotserver.py
@@ -1,6 +1,7 @@
 import numpy as np
 import serial
 import sounddevice as sd
+import scipy.signal as sig
 
 class RobotServer:
     def __init__(self, config = {}, sharedVariables = None ,sharedFrame = None):
@@ -20,6 +21,23 @@ class RobotServer:
         self.freq = self.config['mode3_freq']
         # Recording duration
         self.duration = self.config['mode3_duration']
+        
+        # Mode 2 parameters
+        self.freq_min = 500
+        self.freq_max = 4000
+        self.taille_fenetre = 256 
+        self.pas = 128
+        self.taille_fft = 512 
+        self.Fs = 44100
+        self.duree = 1
+        self.max_spectres_moyen=[]
+        self.nb_bruits_consecutifs = 0
+        self.bruit_detecte = False
+        self.premiere_detection = True
+        self.seuil = None
+        
+
+
     
     def stopRobot(self):
         if self.direction != [0, 0]:
@@ -98,6 +116,62 @@ class RobotServer:
             self.speed = 0
             self.direction = [0, 0]
 
+
+    def mode2Init(self):
+        self.nb_bruits_consecutifs = 0
+        self.bruit_detecte = False
+        self.premiere_detection = True
+        self.seuil = None
+        self.max_spectres_moyen = []
+        
+
+    def mode2Control(self):
+        print("Enregistrement en cours")
+        signal = sd.rec(int(self.duree * self.Fs), samplerate=self.Fs, channels=1)
+        sd.wait()
+
+        f, t, S = sig.spectrogram(signal[:, 0], fs=self.Fs, window='hann', nperseg=self.taille_fenetre,
+                                noverlap=self.taille_fenetre - self.pas, nfft=self.taille_fft, detrend=False)
+
+        freq_bin = np.logical_and(f > self.freq_min, f <= self.freq_max)
+        spectre_moyen = np.mean(np.abs(S[freq_bin, :]), axis=0)
+
+        if self.seuil is None:
+            self.seuil = 20 * np.std(spectre_moyen)
+
+        max_bruit = np.max(spectre_moyen)
+
+        if max_bruit > self.seuil:
+            print('Bruit détecté, fuyons!')
+            if self.premiere_detection:
+                self.premiere_detection = False
+            else:
+                self.nb_bruits_consecutifs += 1
+
+            if self.nb_bruits_consecutifs == 2:
+                print('Trop de bruits détectés, arrêt du programme.')
+            else:
+                self.max_spectres_moyen.append(max_bruit)
+        else:
+            print('Aucun bruit bizarre, restons bien caché!')
+            if not self.premiere_detection:
+                self.bruit_detecte = False
+
+        print("La valeur seuil est :", self.seuil)
+        print("La valeur maximale du bruit est :", max_bruit)
+        print()
+
+        print("Le valeur max des bruit sont :", self.max_spectres_moyen)
+
+        if self.max_spectres_moyen[0] < self.max_spectres_moyen[1]:
+            print("Le bruit augmente.")
+        elif self.max_spectres_moyen[0] > self.max_spectres_moyen[1]:
+            print("Le bruit diminue.")
+        else:
+            print("Le bruit est constant.")
+
+
+
     def mode3Control(self):
         # check if sharedvariable has mode3_record to true
         if 'mode3_record' in self.sharedVariables and self.sharedVariables['mode3_record']:
@@ -122,6 +196,8 @@ class RobotServer:
                     self.manualControl()
                 elif self.sharedVariables['mode'] == 1:
                     self.mode1Control()
+                elif self.sharedVariables['mode'] == 2:
+                    self.mode2Control()
                 elif self.sharedVariables['mode'] == 3:
                     self.mode3Control()
                 else:
