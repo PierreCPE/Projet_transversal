f1d58d3e0d78107062091d3fdee517a4159dcb32
Hugues <hugues.farthouat@cpe.fr>
2023-05-25 09:38:20 +0200

    simu update 2

--- main/site_web/robotdifferentiel.py
+++ main/site_web/robotdifferentiel.py
@@ -1,5 +1,6 @@
 import math
 from simulationsensor import LidarSimulationSensor
+
 class RobotDifferential:
     def __init__(self, carte_largeur, carte_hauteur, robot_largeur, robot_hauteur, vitesse_max, vitesse_rotation_max, config, sharedVariables):
         import pygame        
