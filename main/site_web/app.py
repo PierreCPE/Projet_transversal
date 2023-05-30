dfd072715556fa5bfd0ac78804c1987675befbfe
Hugues <hugues.farthouat@cpe.fr>
2023-05-30 10:44:56 +0200

    re

--- main/site_web/app.py
+++ main/site_web/app.py
@@ -48,7 +48,7 @@ class App:
         ###########################################
         config = ThreadSafeDict()
         config['detection_contour'] = True
-        config['serial'] = False # Activer ou non le port serial
+        config['serial'] = True # Activer ou non le port serial
         # config['serial_port'] = 'COM8' # Port série
         config['serial_port'] = '/dev/ttyUSB1' # Port série
         config['serial_baudrate'] = 19200 # Baudrate du port série
@@ -65,7 +65,7 @@ class App:
         config['auth_failed_limit'] = 7 # Nombre de tentatives de connexion avant de bloquer l'adresse IP
         config['auth_try_time'] = 5 # Temps en secondes avant de pouvoir réessayer de se connecter
         config['simulation_robot'] = False # Activer ou non le robot de simulation
-        config['utilisation_lidar'] = False # Si le lidar est utilisé (pour les tests)
+        config['utilisation_lidar'] = True # Si le lidar est utilisé (pour les tests)
         config['windows'] = False # Si le lidar est utilisé (pour les tests)
         ###########################################
         return config
